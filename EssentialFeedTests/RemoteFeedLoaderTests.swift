7935c6eec2b6ce438eb1c70d1650c0fa0f8d9c40
arupsarkar <sarkar.arup@gmail.com>
2021-04-03 22:39:08 -0400

    Moved test logic to separate type

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,21 +10,37 @@ import XCTest
 class RemoteFeedLoader {
     //nned a collaborator like http client
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        //HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!) //not optional
     }
 }
 // singleton: single point of access
 class HTTPClient {
-    static let shared = HTTPClient()
+    // instead of var, using let would make it singleton
+    // var would allow it to become global var
+    static var shared = HTTPClient()
     
-    //private initializer
-    private init() {}
+    //private initializer not needed if not a singleton
+//    private init() {}
+    
+    func get(from url: URL) {}
+}
+
+//sub class of HTTP client
+class HTTPClientSpy: HTTPClient {
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     var requestedURL: URL? //optional
 }
+
 class RemoteFeedLoaderTests: XCTestCase {
     // test initializer
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        //override the shared property
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         //no url
         XCTAssertNil(client.requestedURL)
@@ -32,7 +48,8 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     // second test
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         //system under test sut
         let sut = RemoteFeedLoader()
         sut.load();
